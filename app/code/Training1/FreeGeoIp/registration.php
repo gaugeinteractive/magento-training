59b2dddc1a3b96a9ec071264dc2e628c892d7892
Nate Holland <nateholland@icloud.com>
2017-05-31 11:26:54 -0400

    Add review filter to throw error on names with dashes

--- app/code/Training1/FreeGeoIp/registration.php
+++ app/code/Training1/FreeGeoIp/registration.php
@@ -4,8 +4,8 @@
  * See COPYING.txt for license details.
  */
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Training1_FreeGeoIp',
-    __DIR__
-);
+// \Magento\Framework\Component\ComponentRegistrar::register(
+//     \Magento\Framework\Component\ComponentRegistrar::MODULE,
+//     'Training1_FreeGeoIp',
+//     __DIR__
+// );
