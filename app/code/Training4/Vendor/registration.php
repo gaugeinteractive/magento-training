d0d600c2a6854a7ceace0d3df733c76a70066e27
Jonathan Willingham <jwillingham1167@gmail.com>
2017-06-23 15:53:02 -0400

    Initial module setup

---
+++ app/code/Training4/Vendor/registration.php
@@ -0,0 +1,11 @@
+
+<?php
+/**
+* Gauge Interactive
+*/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Training4_Vendor',
+    __DIR__
+);
